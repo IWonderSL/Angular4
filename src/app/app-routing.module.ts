dd2e0d9c6d612d047af4c00590d7fd5d93c421e2
CHATHUMI <it20385064@my.sliit.lk>
2024-02-20 16:10:22 +0530

    added wild cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./public-base/public-base.module').then((m) => m.PublicBaseModule),
+  },
+  {
+    path: 'privatebase',
+    loadChildren: () =>
+      import('./private-base/private-base.module').then((m) => m.PrivateBaseModule),
+  },
+  {
+    path: '*',
+    loadChildren: () =>
+      import('./public-base/public-base.module').then((m) => m.PublicBaseModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./public-base/public-base.module').then((m) => m.PublicBaseModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
